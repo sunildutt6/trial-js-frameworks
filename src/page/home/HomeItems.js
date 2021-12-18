c50c08961929c4b815278bd8e68d7378fae7676a
sunil <sunildutt6@hotmail.com>
2021-12-18 11:36:34 +0100

    added

--- src/page/home/HomeItems.js
+++ src/page/home/HomeItems.js
@@ -3,35 +3,39 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { useToggle } from "../../hooks/useToggle";
 
 const StyleDiv = styled.div`
   .card {
-    margin: 0.5rem;
-    padding: 0.5rem;
+    margin: 1.5rem 0;
+  }
+  .card-title {
+    margin: 0.5rem 0;
+    text-align: center;
+  }
+  .card-image {
+    margin-bottom: 1rem;
   }
 `;
 
 function HomeItems({ id, name, status, image }) {
+  const [isFavourite, setIsFavourite] = useToggle();
   return (
     <Col>
-      <NavLink to={`detail/${id}`}>
-        <StyleDiv>
-          <Card style={{ width: "15rem" }} className="card">
-            <Card.Img
-              variant="top"
-              src={image}
-              width={250}
-              height={250}
-              className="mb-3"
-            />
-            <Card.Body>
-              <Card.Title className="card-title">
-                {name.toUpperCase()}
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </StyleDiv>
-      </NavLink>
+      <StyleDiv>
+        <Card style={{ width: "15rem" }} className="card">
+          <NavLink to={`detail/${id}`}>
+            <Card.Title className="card-title">{name.toUpperCase()}</Card.Title>
+          </NavLink>
+          <Card.Body>
+            <Card.Img src={image} className="card-image" />
+            <Button variant="secondary" onClick={setIsFavourite}>
+              {isFavourite ? "Remove" : "Add"}
+            </Button>
+          </Card.Body>
+        </Card>
+      </StyleDiv>
     </Col>
   );
 }
